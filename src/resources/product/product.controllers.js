49d16faa7430c54c32f5a90c738aca4239ca9d1c
Roman <romanbrito1@gmail.com>
2020-06-17 10:38:03 -0500

    talking to stripe to create products

--- src/resources/product/product.controllers.js
+++ src/resources/product/product.controllers.js
@@ -1,11 +1,33 @@
+const stripe = require('stripe')(process.env.STRIPE)
 const Product = require('./product.model')
 
 const controllers = {
   createOne() {
     return async (req, res) => {
       try {
-        const doc = await Product.create({...req.body})
-        res.status(201).json({ data: doc})
+        let email = 'admin@example.com';
+console.log(email.includes('@'));
+        // const doc = await Product.create({...req.body})
+        // res.status(201).json({ data: doc})
+        const {event, entry} = req.body
+        console.log(entry)
+        stripe.products.retrieve(
+          entry.uid,
+          function(err, product) {
+            // asynchronously called
+            if(product) {
+              console.log('product exists', product)
+            } else if(err.code === 'resource_missing') {
+              console.log('creating product')
+              const {product} = entry
+              const stripeProduct = {...product, id: entry.uid}
+              console.log(stripeProduct)
+              // stripe.products.create(product)
+            } else {
+              throw(err)
+            }
+          }
+        )
       } catch(e) {
         console.error(e)
         res.status(400).end()
